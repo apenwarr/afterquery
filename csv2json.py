c5b180602532ea72f642bb3ec2207b81a0433024
Avery Pennarun <apenwarr@gmail.com>
2012-11-26 22:47:49 -0500

    csv2json: assume csv files are in latin1 encoding.
    This probably isn't a reasonable assumption, but it's probably true for
    English-language speakers parsing csv files produced by Excel.

--- csv2json.py
+++ csv2json.py
@@ -4,7 +4,7 @@ import json
 import sys
 
 c = csv.reader(sys.stdin)
-rows = list(c)
+rows = list([[col.decode('latin1') for col in r] for r in c])
 print 'jsonp('
 json.dump(rows, sys.stdout, indent=2)
 print ');'
